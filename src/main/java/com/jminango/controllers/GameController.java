a91244d996abc210bc7e73491dbb673a1f45f8bf
Juan Minango <juan.minango@bbchain.com.br>
2024-11-12 21:57:19 -0300

    GET game by Id

--- src/main/java/com/jminango/controllers/GameController.java
+++ src/main/java/com/jminango/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.jminango.controllers;
 
+import com.jminango.dto.GameDTO;
 import com.jminango.dto.GameMinDTO;
 import com.jminango.servicies.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,11 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll() {
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
     }
 
 }
--- src/main/java/com/jminango/servicies/GameService.java
+++ src/main/java/com/jminango/servicies/GameService.java
@@ -1,10 +1,12 @@
 package com.jminango.servicies;
 
+import com.jminango.dto.GameDTO;
 import com.jminango.dto.GameMinDTO;
 import com.jminango.entities.Game;
 import com.jminango.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,9 +16,16 @@ public class GameService {
     @Autowired
     GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
     }
 
 }
